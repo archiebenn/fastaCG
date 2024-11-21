2c647e31f406b550f3511dd49afd1c500ba5ac56
archie <archiesnbenn@gmail.com>
2024-11-21 19:21:16 +0000

    Modified .sh to relative paths

--- fastaCGWrapper.sh
+++ fastaCGWrapper.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #a bash script for URLs to .fa.gz (only - gzipped atm) files which will calculate the CG content from command line
-# activate venv
-source /home/ab/Dropbox/coding/shellin/fastaCG/venv/bin/activate
 
-#downloads argument link from url (.fa.gz file), renames it yo fasta.fa.gz, then gunzips it (creates a fasta.fa file)
-wget "$1" -O fasta.fa.gz && gunzip fasta.fa.gz
+#get cd
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+
+# activate venv
+source "$script_dir/venv/bin/activate"
+
+#downloads argument link from url (.fa.gz file), renames it to fasta.fa.gz, then gunzips it (creates a fasta.fa file)
+wget "$1" -O "$script_dir/fasta.fa.gz" && gunzip "$script_dir/fasta.fa.gz"
 
 #tells bash to run python script with fasta.fa as the argument (sys.argv[1] in python)
 #note that fasta.fa is the FILENAME (ie string literal), so .py script will need to open that file
-python3 /home/ab/Dropbox/coding/shellin/fastaCG/fastaCG.py fasta.fa
+python3 "$script_dir/fastaCG.py" "$script_dir/fasta.fa"
 
 
 deactivate
