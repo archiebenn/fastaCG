d70930e67050c8481f7bb96f45851c9c4f293a5a
archie <archiesnbenn@gmail.com>
2024-11-21 19:53:24 +0000

    Added data directory

--- fastaCGWrapper.sh
+++ fastaCGWrapper.sh
@@ -8,11 +8,11 @@ script_dir="$(cd "$(dirname "$0")" && pwd)"
 source "$script_dir/venv/bin/activate"
 
 #downloads argument link from url (.fa.gz file), renames it to fasta.fa.gz, then gunzips it (creates a fasta.fa file)
-wget "$1" -O "$script_dir/fasta.fa.gz" && gunzip "$script_dir/fasta.fa.gz"
+wget "$1" -O "$script_dir/data/fasta.fa.gz" && gunzip "$script_dir/data/fasta.fa.gz"
 
 #tells bash to run python script with fasta.fa as the argument (sys.argv[1] in python)
 #note that fasta.fa is the FILENAME (ie string literal), so .py script will need to open that file
-python3 "$script_dir/fastaCG.py" "$script_dir/fasta.fa"
+python3 "$script_dir/fastaCG.py" "$script_dir/data/fasta.fa"
 
 
 deactivate
