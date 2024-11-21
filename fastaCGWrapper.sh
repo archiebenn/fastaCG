f0e2d602dc8707743fe6c0088f21e1ae24f805f2
archie <archiesnbenn@gmail.com>
2024-11-21 21:29:15 +0000

    Adding create venv and install requirements to .sh

--- fastaCGWrapper.sh
+++ fastaCGWrapper.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 #a bash script for URLs to .fa.gz (only - gzipped atm) files which will calculate the CG content from command line
 
-#get cd
+# get cd
 script_dir="$(cd "$(dirname "$0")" && pwd)"
+
+# create venv
+python3 -m venv "$script_dir/venv"
 
 # activate venv
 source "$script_dir/venv/bin/activate"
 
-#downloads argument link from url (.fa.gz file), renames it to fasta.fa.gz, then gunzips it (creates a fasta.fa file)
+# install requirements
+pip install -r "$script_dir/requirements.txt"
+
+# downloads argument link from url (.fa.gz file), renames it to fasta.fa.gz, then gunzips it (creates a fasta.fa file)
 wget "$1" -O "$script_dir/data/fasta.fa.gz" && gunzip "$script_dir/data/fasta.fa.gz"
 
-#tells bash to run python script with fasta.fa as the argument (sys.argv[1] in python)
-#note that fasta.fa is the FILENAME (ie string literal), so .py script will need to open that file
+# tells bash to run python script with fasta.fa as the argument (sys.argv[1] in python)
+# note that fasta.fa is the FILENAME (ie string literal), so .py script will need to open that file
 python3 "$script_dir/fastaCG.py" "$script_dir/data/fasta.fa"
 
 
